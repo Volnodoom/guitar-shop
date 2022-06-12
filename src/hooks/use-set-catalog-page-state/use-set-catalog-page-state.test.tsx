0454027ac2300f81e2cacf3fad91f1caf50b8c01
Alexander <bdiyoz@gmail.com>
2022-06-12 17:56:57 +0400

    test: bug with custome hook test for use-debounced-value

--- src/hooks/use-set-catalog-page-state/use-set-catalog-page-state.test.tsx
+++ src/hooks/use-set-catalog-page-state/use-set-catalog-page-state.test.tsx
@@ -1,39 +1,16 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import { renderHook } from '@testing-library/react-hooks';
-import userEvent from '@testing-library/user-event';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { Provider } from 'react-redux';
-import * as Redux from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
 import { createMockState } from '../../utils/mock-faker';
-import Catalog from '../../components/catalog/catalog';
 import { useSetCatalogPageState } from './use-set-catalog-page-state';
 import React, {FC} from 'react';
 
-// jest.mock('../hook', () => jest.fn());
 
 describe('Custom hook: use-set-catalog-page-state', () => {
-  it('return one element (function)', () => {
-
+  it('Return one element (function)', () => {
     const mockState = createMockState();
     const mockStore = configureMockStore()(mockState);
-    // mockStore.dispatch = jest.fn();
 
-    // const dispatchFunction = jest.fn();
-    // const useDispatch = jest.spyOn(Redux, 'useDispatch');
-    // useDispatch.mockReturnValue(dispatchFunction);
-
-    // const setItemRangeStart = jest.fn();
-    // const setCurrentPage = jest.fn();
-
-
-    // const wrapper = () => (
-    //   <Provider store={mockStore}>
-    //     <MemoryRouter>
-    //       <Catalog />
-    //     </MemoryRouter>
-    //   </Provider>
-    // );
 
     const wrapper: FC<{children: React.ReactNode}> =  ({ children }) => <Provider store={mockStore}>{children}</Provider>;
     const {result} = renderHook(() => useSetCatalogPageState(), {wrapper});
@@ -43,13 +20,33 @@ describe('Custom hook: use-set-catalog-page-state', () => {
     expect(setPageState).toBeInstanceOf(Function);
   });
 
-  // it('return one element (function)', () => {
+  it('Parameter, which have been passed to return function lead to call actions: setItemRangeStart and setCurrentPage', () => {
+    const FAKE_NUMBER = 3;
 
-  // });
-  // it('return one element (function)', () => {
+    const mockState = createMockState();
+    const mockStore = configureMockStore()(mockState);
+    mockStore.dispatch = jest.fn();
 
-  // });
-  // it('return one element (function)', () => {
+    const wrapper: FC<{children: React.ReactNode}> =  ({ children }) => <Provider store={mockStore}>{children}</Provider>;
+    const {result} = renderHook(() => useSetCatalogPageState(), {wrapper});
+    const [setPageState] = result.current;
+    act(() => setPageState(FAKE_NUMBER));
 
-  // });
+    expect(mockStore.dispatch).toHaveBeenCalledTimes(2);
+  });
+
+  it('Passing parameter with value equal to null to return function does not lead to call any dispatch actions', () => {
+    const FAKE_VALUE = null;
+
+    const mockState = createMockState();
+    const mockStore = configureMockStore()(mockState);
+    mockStore.dispatch = jest.fn();
+
+    const wrapper: FC<{children: React.ReactNode}> =  ({ children }) => <Provider store={mockStore}>{children}</Provider>;
+    const {result} = renderHook(() => useSetCatalogPageState(), {wrapper});
+    const [setPageState] = result.current;
+    act(() => setPageState(FAKE_VALUE));
+
+    expect(mockStore.dispatch).toHaveBeenCalledTimes(0);
+  });
 });
